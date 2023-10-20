ca3104abb24304077499394f9640cfb75f0ec340
Vishal Singh <vishalsingh2376@gmail.com>
2023-10-20 15:16:06 +0530

    add ascii value and image to ascii logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,33 @@
 use image::GenericImageView;
 
+fn get_ascii(intent: u8) -> &'static str {
+    let index = intent / 32;
+    let ascii = [" ", ".", ",", "-", "~", "+", "=", "@"];
+    return ascii[index as usize];
+}
+
 fn get_image(dir: &str, scale: u32) {
     let img = image::open(dir).unwrap();
     println!("{:?}", img.dimensions());
     let (width, height) = img.dimensions();
+
+    for y in 0..height {
+        for x in 0..width {
+            if y % (scale * 2) == 0 && x % scale == 0 {
+                let pix = img.get_pixel(x, y);
+                let mut intent = pix[0] / 3 + pix[1] / 3 + pix[2] / 3; // RGB value
+                if pix[3] == 0 {
+                    intent = 0
+                }
+
+                println!("{}", get_ascii(intent));
+            }
+        }
+
+        if y % (scale * 2) == 0 {
+            println!("");
+        }
+    }
 }
 
 fn main() {
