a8bb927a6a64d6a4f664998267615032504de3ba
Vishal Singh <vishalsingh2376@gmail.com>
2023-10-20 15:05:38 +0530

    add get_image() func for image dimensions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use image::GenericImageView;
+
+fn get_image(dir: &str, scale: u32) {
+    let img = image::open(dir).unwrap();
+    println!("{:?}", img.dimensions());
+    let (width, height) = img.dimensions();
+}
+
 fn main() {
-    println!("Hello, world!");
+    get_image("leaf.png", 4);
 }
